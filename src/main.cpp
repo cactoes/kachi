4e155e1e0c083d144c2236a9e7a91655aa108b86
cactoes <maartenhvanrooij@gmail.com>
2024-02-06 19:43:35 +0100

    testing code

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,17 @@
 
 #include "scraper/scraper.hpp"
 
-#define WEBSITE_TEST
+// #define WEBSITE_TEST
 
 int main() {
 #ifdef WEBSITE_TEST
     httplib::Client client("https://www.livechart.me");
     auto res = client.Get("/timetable");
 
-    auto start = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < 10; i++)
-        scraper::ParseHTML(res->body);
-    auto stop = std::chrono::high_resolution_clock::now();
+    scraper::HTMLElement document = scraper::ParseHTML(res->body);
+
+    auto elements = document.GetElementsByClassname("timetable-day");
+
 #else
     std::string testHTML =
         "<hmtl>"
@@ -25,19 +25,16 @@ int main() {
                "<link rel=\"dns-prefetch\" href=\"https://s.livechart.me\">"
             "</head>"
             "<body>"
-                "<p>"
+                "<p id=\"testId\">"
                     "Test"
                 "</p>"
-                "<input class=\"hide\" type=\"checkbox\" name=\"rewatching\" data-mark-filter-modal-target=\"checkbox\" />"
+                "<input class=\"hide button\" type=\"checkbox\" name=\"rewatching\" data-mark-filter-modal-target=\"checkbox\" />"
             "</body>"
         "</hmtl>";
 
     scraper::HTMLElement document = scraper::ParseHTML(testHTML);
+    auto elements = document.GetElementById("testId");
+
 #endif
-
-    auto duration = duration_cast<std::chrono::milliseconds>(stop - start);
-
-    std::cout << (double)(duration.count()) / 10.0 << std::endl;
-
     return 0;
 }
